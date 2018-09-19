aafa5eeb9c63b602999343e0e51e78bb5a69eaa8
Gabriel <gabrielj.harel@gmail.com>
2018-09-19 18:46:19 -0400

    rewrite implementation

--- index.ts
+++ index.ts
@@ -1,91 +1,100 @@
+import "isomorphic-fetch";
 import express from "express";
 
 // prettier-ignore
-type httpVerb = "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" | "PATCH";
+export type Method = "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" | "PATCH";
 // prettier-ignore
-type statusCode = 100 | 101 | 102 | 103 | 200 | 201 | 202 | 203 | 204 | 205 | 206 | 207 | 208 | 226 | 300 | 301 | 302 | 303 | 304 | 305 | 306 | 307 | 308 | 400 | 401 | 402 | 403 | 404 | 405 | 406 | 407 | 408 | 409 | 410 | 411 | 412 | 413 | 414 | 415 | 416 | 417 | 418 | 421 | 422 | 423 | 424 | 426 | 428 | 429 | 431 | 451 | 500 | 501 | 502 | 503 | 504 | 505 | 506 | 507 | 508 | 510 | 511;
+export type Status = 100 | 101 | 102 | 103 | 200 | 201 | 202 | 203 | 204 | 205 | 206 | 207 | 208 | 226 | 300 | 301 | 302 | 303 | 304 | 305 |  306 | 307 | 308 | 400 | 401 | 402 | 403 | 404 | 405 | 406 | 407 | 408 | 409 | 410 | 411 | 412 | 413 | 414 | 415 | 416 | 417 | 418 | 421 |  422 | 423 | 424 | 426 | 428 | 429 | 431 | 451 | 500 | 501 | 502 | 503 | 504 | 505 | 506 | 507 | 508 | 510 | 511;
 
-export interface Contract<T = any, S = any> {
-    request: T;
-    response: S;
+export interface Config {
+    method?: Method;
+    host?: string;
+    path: string;
+    expect?: Status | Status[];
 }
 
-interface EndpointConfig {
-    verb: httpVerb;
-    expectedStatusCode: statusCode[];
+interface StrictConfig {
+    method: Method;
+    host: string;
+    path: string;
+    expect: Status[];
 }
 
-export class Endpoint<C extends Contract> {
-    public readonly config: EndpointConfig;
-    public readonly path: string;
-
-    constructor(path: string, config?: EndpointConfig) {
-        const defaultConfig: EndpointConfig = {
-            verb: "POST",
-            expectedStatusCode: [200],
-        };
-        this.config = Object.assign(defaultConfig, config);
-        this.path = path;
-    }
-
-    public async call(req: C["request"]): Promise<C["response"]> {
-        console.log(req);
-        return {};
-    }
-
-    public register(
-        app: express.Application,
-        handler: (
-            requestData: C["request"],
-            req: express.Request,
-            res: express.Response,
-        ) => Promise<C["response"]>,
-        errorHandler?: (err: any) => any,
-    ): void {
-        const verb = this.config.verb.toLowerCase();
-        const test: express.IRouterMatcher<any> = (app as any)[verb] as any;
-
-        test(this.path, async (req, res) => {
-            if (res.headersSent) {
-                return;
-            }
-
-            const rawRequestData = await new Promise<string>((resolve) => {
-                let data = "";
-                req.setEncoding("utf8");
-                req.on("data", (chunk) => (data += chunk));
-                req.on("end", () => resolve(data));
-            });
-
-            let requestData = null;
-            try {
-                requestData = JSON.parse(rawRequestData);
-            } catch (e) {
-                res.sendStatus(400);
-                res.send("Could not parse incoming request data.");
-                return;
-            }
-
-            let responseData = null;
-            try {
-                responseData = await handler(requestData, req, res);
-            } catch (e) {
-                res.sendStatus(500);
-                res.send("Request handler error.");
-                return;
-            }
-
-            let rawResponseData = "";
-            try {
-                rawResponseData = JSON.stringify(responseData);
-            } catch (e) {
-                res.sendStatus(500);
-                res.send("Could not serialize outgoing response data.");
-                return;
-            }
-
-            res.sendStatus(200);
-            res.send(rawResponseData);
-        });
-    }
+export interface RequestHandler<RQ, RS> {
+    (data: RQ, req: express.Request, res: express.Response): RS | Promise<RS>;
 }
+
+export interface Endpoint<RQ, RS> {
+    config: StrictConfig;
+    (handler: RequestHandler<RQ, RS>): express.RequestHandler;
+    (data: RQ): Promise<RS>;
+}
+
+const respond = (
+    config: StrictConfig,
+    handler: RequestHandler<any, any>,
+): express.RequestHandler => async (req, res, next) => {
+    if (req.path !== config.path) {
+        return next();
+    }
+    if (req.method !== config.method) {
+        return next();
+    }
+    if (res.headersSent) {
+        return next();
+    }
+
+    const rawRequestData = await new Promise<string>((resolve) => {
+        let data = "";
+        req.setEncoding("utf8");
+        req.on("data", (chunk) => (data += chunk));
+        req.on("end", () => resolve(data));
+    });
+
+    const requestData = JSON.parse(rawRequestData);
+    const responseData = await handler(requestData, req, res);
+    const rawResponseData = JSON.stringify(responseData);
+
+    if (!res.headersSent) {
+        res.status(200);
+        res.send(rawResponseData);
+    }
+    next();
+};
+
+const request = async (config: StrictConfig, data: any) => {
+    const response = await fetch(config.host + config.path, {
+        method: config.method,
+        body: JSON.stringify(data),
+    });
+
+    if (config.expect.indexOf(response.status as any) < 0) {
+        throw new Error("Unexpected status");
+    }
+
+    return response.json();
+};
+
+const hammond = <RQ, RS>(pathOrConfig: Config | string): Endpoint<RQ, RS> => {
+    if (typeof pathOrConfig === "string") {
+        pathOrConfig = {path: pathOrConfig};
+    }
+
+    const config: StrictConfig = {
+        path: pathOrConfig.path,
+        host: pathOrConfig.host || "",
+        method: pathOrConfig.method || "POST",
+        expect: [].concat(pathOrConfig.expect || (200 as any)) as Status[],
+    };
+
+    const callable = (dataOrHandler: any): any => {
+        if (typeof dataOrHandler === "function") {
+            return respond(config, dataOrHandler);
+        }
+        return request(config, dataOrHandler);
+    };
+
+    return Object.assign(callable, {config});
+};
+
+export default hammond;
